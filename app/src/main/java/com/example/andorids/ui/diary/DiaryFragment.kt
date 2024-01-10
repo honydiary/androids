83ddddc9f09530ac2c1d98eb3735cc8fee9d11e2
hom237 <87473852+hom237@users.noreply.github.com>
2024-01-10 22:21:15 +0900

    refactor all code

--- app/src/main/java/com/example/andorids/ui/diary/DiaryFragment.kt
+++ app/src/main/java/com/example/andorids/ui/diary/DiaryFragment.kt
@@ -1,4 +1,4 @@
-package com.example.andorids.ui.notifications
+package com.example.andorids.ui.diary
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.andorids.databinding.FragmentNotificationsBinding
+import com.example.andorids.databinding.FragmentDiaryBinding
 
-class NotificationsFragment : Fragment() {
+class DiaryFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentDiaryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +22,12 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val diaryViewModel =
+            ViewModelProvider(this).get(DiaryViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentDiaryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
